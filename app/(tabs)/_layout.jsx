674f4a7a93b0d810085667989a394cd6652cb600
Shiran <xiao.shiran@gmail.com>
2024-07-10 15:42:29 -0400

    index done, start home screen

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,80 @@
+import { StatusBar } from "expo-status-bar";
+import { Tabs } from "expo-router";
+import { Image, Text, View } from "react-native";
+
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name="Create"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+};
+
+export default TabLayout;
---
+++ app/index.jsx
@@ -0,0 +1,55 @@
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import { CustomButton, Loader } from "../components";
+
+const Welcome = () => {
+
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Text className="text-3xl text-white font-bold text-center text-secondary-200">Vertafore</Text>
+
+          <Image
+            source={images.empty}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover with{" "}
+              <Text className="text-secondary-200">Vertafore</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[226px] h-[10px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <CustomButton
+            title="Let's start!"
+            handlePress={() => router.push("/home")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
